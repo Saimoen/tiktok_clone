737fc64f303fdd6a26f3be1f49cd52f4ce4c72db
Gregory Saimoen <greg.eres.07@hotmail.com>
2022-07-12 09:50:01 +1100

    final commit

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -57,7 +57,7 @@ const Detail = ({ postDetails }: IProps) => {
     }
   }
 
-  const addComment = async (e) => {
+  const addComment = async (e: { preventDefault: () => void }) => {
     e.preventDefault()
 
     if(userProfile && comment) {
